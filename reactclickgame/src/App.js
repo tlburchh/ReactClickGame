c9d70c3045d616c507aa85310f7133d9c466ea24
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-10-18 15:44:18 -0400

    added hrefs for images and cleaned up code

--- reactclickgame/src/App.js
+++ reactclickgame/src/App.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/AlphabateCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import alphabet from "./selection.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.alphabet to the alphabet json array
+  state = {
+    alphabet
+  };
+
+  removeFriend = id => {
+    // Filter this.state.alphabet for alphabet with an id not equal to the id being removed
+    const alphabet = this.state.alphabet.filter(friend => friend.id !== id);
+    // Set this.state.alphabet equal to the new alphabet array
+    this.setState({ alphabet });
+  };
+
+  // Map over this.state.alphabet and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>alphabet List</Title>
+        {this.state.alphabet.map(letter => (
+          <FriendCard
+            removeFriend={this.removeLetter}
+            id={letter.id}
+            key={letter.id}
+            name={letter.name}
+            // image={letter.image}
+
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
